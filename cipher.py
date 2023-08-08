d3ef0bc7d22ca6bbdaae20e9817127a4f21b04e5
Ailene Bonczkowski <a.boncz@gmail.com>
2023-08-08 18:58:36 -0400

    cleaned up unnecessary loop

--- cipher.py
+++ cipher.py
@@ -1,25 +1,15 @@
 # create a function to encode a message
-def caesar_decode(message,offset):
+def caesar_decode(message=input("Please enter a message: "), offset=int(input("Enter the shift number"))):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     decoded_message = ''
     for char in message.lower():
         if char in alphabet:
             index = alphabet.find(char)
-            new_index = index + offset + 1
-            #account for numbers greater than 26
-            if new_index > 26:
-                new_index = new_index%26
-            new_char = alphabet[new_index-1]
+            new_index = (index + offset) % 26
+            new_char = alphabet[new_index]
             decoded_message+=new_char
         else:
             decoded_message+=char
     return decoded_message
 
-#test with an offset of 5
-message = "python is fun!"
-offset = 5
-print(caesar_decode(message,offset))
-
-#test with an offset larger than 26
-offset = 29
-print(caesar_decode(message,offset))
+print(caesar_decode())
